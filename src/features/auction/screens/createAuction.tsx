acfaa5594a7f020410ff296807e7006bcc333c34
Mawuli Kwadzofio <34554163+Prince-Kd@users.noreply.github.com>
2023-05-02 23:02:33 +0000

    update create auction app

--- src/features/auction/screens/createAuction.tsx
+++ src/features/auction/screens/createAuction.tsx
@@ -1,10 +1,37 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import AppBar from "../../../components/appBar";
+import { TextInput } from "react-native-paper";
+import FloatingBottomTab from "../../../components/floatingBottomTab";
+
 
 export default function CreateAuction() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View></View>
+    <SafeAreaView style={{ flex: 1, position: "relative" }}>
+      <ScrollView style={{ paddingTop: 10, flex: 1 }}>
+        <AppBar title="Create Auction" />
+        <View style={{ paddingHorizontal: 20, paddingTop: 30 }}>
+          <Text style={{ fontSize: 16, fontWeight: "500" }}>
+            Choose a category
+          </Text>
+          <TextInput
+            placeholder="Looking for something?"
+            mode="outlined"
+            outlineStyle={{ borderRadius: 25, borderColor: "transparent" }}
+            style={{
+              backgroundColor: "#eeeeee",
+              marginBottom: 20,
+              height: 50,
+            }}
+            right={<TextInput.Icon icon={"magnify"} />}
+          />
+        </View>
+      </ScrollView>
+      <FloatingBottomTab />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+
+});
--- src/navigation/Main/mainNavigator.tsx
+++ src/navigation/Main/mainNavigator.tsx
@@ -5,11 +5,12 @@ import { useBottomNavStore } from "../../hooks/useBottomNavStore";
 import Favorite from "../../features/favorite/screens/favorite";
 import Profile from "../../features/profile/screens/profile";
 import HomeStack from "./homeStack";
+import CreateAuction from "../../features/auction/screens/createAuction";
 
 const Stack = createNativeStackNavigator();
 
 export default function MainStackNavigator() {
   const activeIndex = useBottomNavStore((state) => state.activeIndex);
-  const routes = [<HomeStack />, <Favorite />, <Favorite />, <Favorite />, <Profile />];
+  const routes = [<HomeStack />, <Favorite />, <CreateAuction />, <Favorite />, <Profile />];
   return routes[activeIndex];
 }
