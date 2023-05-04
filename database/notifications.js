849d388c2f2eae494855da4f30cfac5373191298
joesikah <42651902+joesikah@users.noreply.github.com>
2023-05-04 12:06:33 +0000

    Notifications

---
+++ database/notifications.js
@@ -0,0 +1,56 @@
+export const notification_list = [
+  {
+    title: "Auction Closed",
+    description: "Your auction closed on $95,000 at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Closed",
+    description: "Your auction closed on $95,000 at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Created",
+    description:
+      "Your auction as created with $95,000 as starting price at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Closed",
+    description: "Your auction closed on $95,000 at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Created",
+    description:
+      "Your auction as created with $95,000 as starting price at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Closed",
+    description: "Your auction closed on $95,000 at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Created",
+    description:
+      "Your auction as created with $95,000 as starting price at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Closed",
+    description: "Your auction closed on $95,000 at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Created",
+    description:
+      "Your auction as created with $95,000 as starting price at 3:15pm today",
+    time: "5 mins ago",
+  },
+  {
+    title: "Auction Closed",
+    description: "Your auction closed on $95,000 at 3:15pm today",
+    time: "5 mins ago",
+  },
+];
--- src/features/notifications/screens/notifications.tsx
+++ src/features/notifications/screens/notifications.tsx
@@ -1,13 +1,62 @@
-import { View, StyleSheet } from "react-native";
+import { View, ScrollView, StyleSheet, Text } from "react-native";
 import AppBar from "../../../components/appBar";
 import FloatingBottomTab from "../../../components/floatingBottomTab";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons } from "@expo/vector-icons";
+import { notification_list } from "../../../../database/notifications";
 
 export default function Notifications() {
   return (
     <SafeAreaView style={{ position: "relative", flex: 1 }}>
       <View style={styles.container}>
         <AppBar title="Notifications" />
+
+        <ScrollView style={{ marginTop: 10, paddingHorizontal: 20 }}>
+          {notification_list.map((_dd, i) => (
+            <View
+              key={i}
+              style={{
+                height: 80,
+                width: "100%",
+                flexDirection: "row",
+                alignItems: "center",
+                borderBottomWidth: 0.2,
+                borderBottomColor: "#eeeee",
+                columnGap: 10,
+              }}
+            >
+              {/* icon */}
+              <View
+                style={{
+                  height: 40,
+                  width: 40,
+                  borderRadius: 100,
+                  backgroundColor: "black",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons
+                  name="notifications-active"
+                  size={24}
+                  color="white"
+                />
+              </View>
+
+              {/* text */}
+              <View style={{ paddingRight: 20 }}>
+                <Text style={{ fontWeight: "bold", fontSize: 16 }}>
+                  {_dd.title}
+                </Text>
+                <Text style={{ fontSize: 12 }}>{_dd.description}</Text>
+                <Text style={{ fontSize: 10, color: "gray", marginTop: 2 }}>
+                  {_dd.time}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
 
         <FloatingBottomTab />
       </View>
